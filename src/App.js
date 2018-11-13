7f93f1da98ec42e6c7f8297548f38c88788ff5af
David Gomez <dg.dave@gmail.com>
2018-11-13 09:03:06 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import ChartWrapper from './components/ChartWrapper'
+import { Bar, Donut, GroupedBar, Line, Sparkline, StackedArea, StackedBar, Step, Tooltip } from 'britecharts-react'
+import barData from './json/barData.json'
+import donutData from './json/donutData.json'
+import groupedBarData from './json/groupedBarData.json'
+import lineData from './json/lineData.json'
+import sparklineData from './json/sparklineData.json'
+import stackedAreaData from './json/stackedAreaData.json'
+import stackedBarData from './json/stackedBarData.json'
+import stepData from './json/stepData.json'
 import './App.css';
+import './styles/charts/bar.css'
+import './styles/charts/donut.css'
+import './styles/charts/grouped-bar.css'
+import './styles/charts/line.css'
+import './styles/charts/sparkline.css'
+import './styles/charts/stacked-area.css'
+import './styles/charts/stacked-bar.css'
+import './styles/charts/step.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const customProps = {
+  width: 600,
+  height: 350,
+}
+
+const renderLine = props => (
+  <Line tooltipThreshold={0} {...props} {...customProps} />
+)
+
+const App = () => {
+  console.log();
+  return (
+    <Fragment>
+      <ChartWrapper>
+        <Bar data={barData.data} {...customProps} />
+      </ChartWrapper>
+      <ChartWrapper>
+        <Donut data={donutData.data} {...customProps} />
+      </ChartWrapper>
+      <ChartWrapper>
+        <GroupedBar data={groupedBarData.data} {...customProps} />
+      </ChartWrapper>
+      <ChartWrapper>
+        <Tooltip data={lineData}
+          render={renderLine}
+          topicLabel={'topics'}
+          title={'React 15'}
+        />
+      </ChartWrapper>
+      <ChartWrapper>
+        <Sparkline data={sparklineData.data} {...customProps} />
+      </ChartWrapper>
+      <ChartWrapper>
+        <StackedArea data={stackedAreaData.data} {...customProps} />
+      </ChartWrapper>
+      <ChartWrapper>
+        <StackedBar data={stackedBarData.data} {...customProps} />
+      </ChartWrapper>
+      <ChartWrapper>
+        <Step data={stepData.data} {...customProps} />
+      </ChartWrapper>
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/components/ChartWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChartWrapper = ({ children }) => (
+  <div className={'wrapper'}>
+    {children}
+  </div>
+)
+
+export default ChartWrapper
